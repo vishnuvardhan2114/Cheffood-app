c24d2354f02b49c3e43e5a46a8af7ad2a9ae3022
vishnuvardhan <134271319+vishnuvardhan2114@users.noreply.github.com>
2024-03-03 03:46:01 +0530

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const App = () => {
   return (
     <div className="container">
@@ -14,6 +15,17 @@ const App = () => {
   );
 };
 
+const AppRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={AppRouter} />);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { LOGO_URL } from '../utils/constants';
+import React from "react";
+import { LOGO_URL } from "../utils/constants";
 const Header = () => {
-    return(
-        <div className='header'>
-            <div className='logo-container'>
-                <img className='logo' src={LOGO_URL} />
-            </div>
-            <div className='nav-items'>
-                <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Contact</li>
-                    <li>cart</li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact</li>
+          <li>cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-export default Header
+export default Header;
